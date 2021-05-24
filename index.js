bf67645bf106049f4e8f51f6243361051068e6bf
wowkster <adyowowk@gmail.com>
2021-05-24 09:14:36 -0400

    added another console log

--- index.js
+++ index.js
@@ -10,26 +10,25 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 
 app.post("/webhook", async (req, res) => {
-    //console.log(`Request made from ${req.ip}`)
- const Payload = req.body;
-//Respond To Heroku Webhook
- res.sendStatus(200);
+    const Payload = req.body;
 
- const options = {
-  method: "POST",
-  url:
-   "https://discord.com/api/webhooks/846362498952986646/LXK4yC-jN7qEqw_ZIOMKyEhf8v705SAB896XRdKyE6PCWW2Iz_IgdDXiWMjkQPpjuAYb",
-  headers: {
-   "Content-type": "application/json",
-  },
-//Format JSON DATA
-  body: JSON.stringify({
-   content: `This is A Webhook notification! A build for your app ${Payload.data.app.name} was just triggered`,
-  }),
- };
- request(options, function (error, response) {
-  if (error) throw new Error(error);
-  //console.log(response);
- });
+    console.log(Payload)
+
+    res.sendStatus(200);
+
+    const options = {
+        method: "POST",
+        url: "https://discord.com/api/webhooks/846362498952986646/LXK4yC-jN7qEqw_ZIOMKyEhf8v705SAB896XRdKyE6PCWW2Iz_IgdDXiWMjkQPpjuAYb",
+        headers: {
+            "Content-type": "application/json",
+        },
+        //Format JSON DATA
+        body: JSON.stringify({
+            content: `This is A Webhook notification! A build for your app ${Payload.data.app.name} was just triggered`,
+        }),
+    };
+    request(options, function (error, response) {
+         (error) throw new Error(error);
+    });
 });
 app.listen(PORT, () => console.log(`App is running on port ${PORT}!`));
