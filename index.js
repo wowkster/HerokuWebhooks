622911534d1f05501803b952719ae33633f0b994
wowkster <adyowowk@gmail.com>
2021-05-24 09:35:32 -0400

    added better embeds

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.post("/webhook", async (req, res) => {
 
     res.sendStatus(200);
 
+    const webhookContent = getWebhookData(Payload)
+
     const options = {
         method: "POST",
         url: "https://discord.com/api/webhooks/846362498952986646/LXK4yC-jN7qEqw_ZIOMKyEhf8v705SAB896XRdKyE6PCWW2Iz_IgdDXiWMjkQPpjuAYb",
@@ -23,12 +25,46 @@ app.post("/webhook", async (req, res) => {
             "Content-type": "application/json",
         },
         //Format JSON DATA
-        body: JSON.stringify({
-            content: `This is A Webhook notification! A build for your app ${Payload.data.app.name} was just triggered`,
-        }),
+        body: JSON.stringify(webhookContent),
     };
     request(options, function (error, response) {
         if (error) throw new Error(error);
     });
 });
 app.listen(PORT, () => console.log(`App is running on port ${PORT}!`));
+
+function getWebhookData(Payload) {
+    if (Payload.resource === 'build'){
+        const succeeded = (Payload.data.status === 'succeeded')
+        const email = Payload.actor.email
+        const commit = Payload.data.slug.commit
+        const app = Payload.data.app.name
+
+        return {
+            "content": null,
+            "embeds": [
+              {
+                "title": "Build Notification",
+                "description": `A build for your app [\`${app}\`](https://dashboard.heroku.com/apps/${app}) was just triggered`,
+                "color": (succeeded ? "#58ff6c" : "#ff5858"),
+                "fields": [
+                  {
+                    "name": "User",
+                    "value": `\`${email}\``
+                  },
+                  {
+                    "name": "Status",
+                    "value": (succeeded ? "Succeeded" : "Failed")
+                  },
+                  {
+                    "name": "Commit",
+                    "value": `ID: [\`${commit}\`](https://github.com/wowkster/ExeMinion/commit/${commit})`
+                  }
+                ]
+              }
+            ]
+          }
+    } else if (Payload.resource === 'dyno') {
+
+    }
+}
